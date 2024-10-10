e3989c396e24d38ef777127589a652004ce73e5b
touhidul-islam-html <ti360109@gmail.com>
2024-10-10 23:18:30 +0600

    update

--- src/component/BottomNav.jsx
+++ src/component/BottomNav.jsx
@@ -131,7 +131,7 @@ const BottomNav = () => {
               type="button"
               className="inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50 dark:hover:bg-gray-800 group"
             >
-              <WalletMinimal  className="dark:text-gray-400 text-gray-500"/>
+              <WalletMinimal  className="dark:text-gray-400 text-gray-500 group-hover:text-blue-600 dark:group-hover:text-blue-500"/>
 
               <span className="text-sm dark:text-gray-400">Budget</span>
             </Link>
--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,133 +1,137 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { useDispatch } from "react-redux";
 import { addIncomeData } from "../redux/productSlice";
 import { useState } from "react";
 
 const Income = () => {
-    const [source, setSource] = useState('');
-    const [amount, setAmount] = useState('');
-    const [date, setDate] = useState('');
-    const [note, setNote] = useState('');
+  const [source, setSource] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  const [note, setNote] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const addIncome = (e) => {
-        e.preventDefault();
-        dispatch(addIncomeData({
-            source: source,
-            amount: amount,
-            date: date,
-            note: note,
-        }));
-    };
-
-    const handleClick = (e) => {
-        e.target.type = 'date'; // Convert input to date type on first click
-        e.target.click();       // Programmatically trigger the click to open date picker
-    };
-
-    const handleBlur = (e) => {
-        if (!e.target.value) {
-            e.target.type = 'text'; // If no date is selected, revert to text input
-        } else {
-            const selectedDate = new Date(e.target.value);
-            e.target.type = 'text';
-            e.target.value = selectedDate.toISOString().substring(0, 10); // Format as YYYY-MM-DD
-            setDate(e.target.value); // Set the date in state
-        }
-    };
-
-    return (
-        <section className="bg-gray-2 pt-[90px] dark:bg-dark lg:pb-20 lg:pt-[120px] pb-10 mb-5">
-            <div className="container">
-                <div className="grid grid-cols-2 gap-3 dark:text-white mt-3 text-xl font-bold">
-                    <h1>Add Income</h1>
-                </div>
-            </div>
-            <section className="z-10 overflow-hidden bg-white lg:py-[120px] mt-6">
-                <div className="">
-                    <div className="-mx-4 flex flex-wrap lg:justify-between">
-                        <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
-                            <div className="bg-white p-8 shadow-lg dark:bg-dark-2 sm:p-12">
-                                <form onSubmit={addIncome}>
-                                    <ContactInputBox
-                                        type="text"
-                                        name="incomeSource"
-                                        placeholder="Income Source"
-                                        data={(e) => setSource(e.target.value)}
-                                        value={source}
-                                    />
-                                    <ContactInputBox
-                                        type="number"
-                                        name="amount"
-                                        placeholder="Amount"
-                                        data={(e) => setAmount(e.target.value)}
-                                        value={amount}
-                                    />
-
-                                    <input 
-                                        type="text" 
-                                        id="dateInput" 
-                                        placeholder="Select a date" 
-                                        className="custom-date-input w-full rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
-                                        onClick={handleClick} 
-                                       /*  onBlur={handleBlur} */
-                                        value={date}
-                                        onChange={(e) => setDate(e.target.value)}
-                                    />
-
-                                    <ContactTextArea
-                                        row="3"
-                                        placeholder="Note"
-                                        name="note"
-                                        data={(e) => setNote(e.target.value)}
-                                        value={note}
-                                    />
-                                    <div>
-                                        <button
-                                            type="submit"
-                                            className="w-full rounded border border-primary bg-primary p-3 text-white transition hover:bg-opacity-90"
-                                        >
-                                            Add Income
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </section>
+  const addIncome = (e) => {
+    e.preventDefault();
+    dispatch(
+      addIncomeData({
+        source: source,
+        amount: amount,
+        date: date,
+        note: note,
+      })
     );
+  };
+
+  return (
+    <section className="bg-gray-2 pt-[90px] dark:bg-dark lg:pb-20 lg:pt-[120px] pb-10 mb-5">
+      <div className="container">
+        <div className="grid grid-cols-2 gap-3 dark:text-white mt-3 text-xl font-bold">
+          <h1>Add Income</h1>
+        </div>
+      </div>
+      <section className="z-10 overflow-hidden bg-white lg:py-[120px] mt-6">
+        <div className="">
+          <div className="-mx-4 flex flex-wrap lg:justify-between">
+            <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
+              <div className="bg-white p-8 shadow-lg dark:bg-dark-2 sm:p-12">
+                <form onSubmit={addIncome}>
+                  <ContactInputBox
+                    type="text"
+                    name="incomeSource"
+                    placeholder="Income Source"
+                    data={(e) => setSource(e.target.value)}
+                    value={source}
+                  />
+                  <ContactInputBox
+                    type="number"
+                    name="amount"
+                    placeholder="Amount"
+                    data={(e) => setAmount(e.target.value)}
+                    value={amount}
+                  />
+                  <DateInputBox />
+                  <ContactTextArea
+                    row="3"
+                    placeholder="Note"
+                    name="note"
+                    data={(e) => setNote(e.target.value)}
+                    value={note}
+                  />
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full rounded border border-primary bg-primary p-3 text-white transition hover:bg-opacity-90"
+                    >
+                      Add Income
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Income;
 
 const ContactInputBox = ({ type, placeholder, name, data, value }) => {
-    return (
-        <div className="mb-6">
-            <input
-                type={type}
-                placeholder={placeholder}
-                name={name}
-                onChange={data}
-                value={value}
-                className="w-full rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
-            />
-        </div>
-    );
+  return (
+    <div className="mb-6">
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        onChange={data}
+        value={value}
+        className="w-full rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
+      />
+    </div>
+  );
+};
+const DateInputBox = () => {
+  const [selectedDate, setSelectedDate] = useState("");
+
+  const handleDateChange = (e) => {
+    const dateValue = e.target.value;
+    setSelectedDate(dateValue.split("-").reverse().join("/")); // Converts the date to dd/mm/yyyy format
+  };
+
+  return (
+    <>
+      <div className="w-full">
+        <label htmlFor="dateinput" className="w-full ">
+          <div className="rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6">
+            {selectedDate ? selectedDate : "dd/mm/yyyy"}
+          </div>
+        </label>
+        <input
+          className=" h-0 bg-transparent outline-none focus:ring-0 ring-0 focus:outline-none w-0 p-0 m-0 border-0"
+          type="date"
+          name="dateinput"
+          id="dateinput"
+          onChange={handleDateChange}
+        />
+      </div>
+    </>
+  );
 };
 
 const ContactTextArea = ({ row, placeholder, name, data, value }) => {
-    return (
-        <div className="mb-6">
-            <textarea
-                rows={row}
-                placeholder={placeholder}
-                name={name}
-                className="w-full resize-none rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
-                onChange={data}
-                value={value}
-            />
-        </div>
-    );
+  return (
+    <div className="mb-6">
+      <textarea
+        rows={row}
+        placeholder={placeholder}
+        name={name}
+        className="w-full resize-none rounded border border-stroke px-[14px] py-3 text-base text-body-color outline-none focus:border-primary dark:border-dark-3 dark:bg-dark dark:text-dark-6"
+        onChange={data}
+        value={value}
+      />
+    </div>
+  );
 };
