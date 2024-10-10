580059b3642a22bffe3f6b27de9e97c45c9e1ff2
touhidul-islam-html <ti360109@gmail.com>
2024-10-10 11:29:28 +0600

    update

--- src/component/SettingButton.jsx
+++ src/component/SettingButton.jsx
@@ -3,10 +3,11 @@ const SettingButton = ({content, icon, onClick}) => {
   return (
     <>
       <div className="w-full">
+      {/* text-dark border-b border-[#E8E8E8] bg-white dark:border-dark dark:bg-dark-4 dark:text-dark-7 py-5 px-2 text-center text-base font-medium */}
         <button
           onClick={onClick}
           type="button"
-          className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2 gap-2 w-full cursor-pointer  "
+          className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2 gap-2 w-full cursor-pointer  "
         >
           {icon}
           <p>{content}</p>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,24 +1,47 @@
 import SettingButton from "../component/SettingButton";
-import { CalendarDays, KeyRound, ListChecks, Moon, UserRoundPen } from "lucide-react";
+import {
+  CalendarDays,
+  KeyRound,
+  ListChecks,
+  Moon,
+  UserRoundPen,
+} from "lucide-react";
 
 const Settings = () => {
-    return (
-        <section className="bg-gray-2 pt-[90px] dark:bg-dark lg:pb-20 lg:pt-[120px] pb-5">
-        <div className="container">
-            <div className="grid grid-cols-2 gap-3 text-white">
-              <h1>Settings</h1>
-            </div>
-            <div className="flex flex-col gap-2 py-10">
-            <SettingButton content={"Dark Mode"} icon={<Moon />
-} onClick={""}/>
-            <SettingButton content={"Change Password"} icon={<KeyRound />} onClick={""}/>
-            <SettingButton content={"Edit Profile"} icon={<UserRoundPen />} onClick={""}/>
-            <SettingButton content={"Calendar"} icon={<CalendarDays />} onClick={""}/>
-            <SettingButton content={"Payment Categories"} icon={<ListChecks />} onClick={""}/>
-            </div>
+  return (
+    <section className="bg-gray-2 pt-[90px] dark:bg-dark lg:pb-20 lg:pt-[120px] pb-5">
+      <div className="container">
+        <div className="grid grid-cols-2 gap-3 text-white">
+          <h1>Settings</h1>
         </div>
+        <div className="flex flex-col gap-2 py-10">
+
+          <SettingButton content={"Dark Mode"} icon={<Moon />} onClick={""} />
+          {/* <ThemeButton/> */}
+          <SettingButton
+            content={"Change Password"}
+            icon={<KeyRound />}
+            onClick={""}
+          />
+          <SettingButton
+            content={"Edit Profile"}
+            icon={<UserRoundPen />}
+            onClick={""}
+          />
+          <SettingButton
+            content={"Calendar"}
+            icon={<CalendarDays />}
+            onClick={""}
+          />
+          <SettingButton
+            content={"Payment Categories"}
+            icon={<ListChecks />}
+            onClick={""}
+          />
+        </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Settings;
