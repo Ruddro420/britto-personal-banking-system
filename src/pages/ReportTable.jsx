ebf0578376eecad4c1b172d3060832ce3ad34073
touhidul-islam-html <ti360109@gmail.com>
2024-10-12 01:40:20 +0600

    update

--- src/pages/ReportTable.jsx
+++ src/pages/ReportTable.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-children-prop */
 import { useDispatch } from "react-redux";
 import { deleteData } from "../redux/productSlice";
 import { Trash2 } from "lucide-react";
+import { useState } from "react";
 
 /* eslint-disable react/prop-types */
 const ReportTable = ({ tabledata, type }) => {
@@ -37,26 +39,40 @@ const ReportTable = ({ tabledata, type }) => {
                 >
                   <th
                     scope="row"
-                    style={{ wordWrap:"break-word" }}
-                    className="px-2 py-4 dark:bg-gray-800 bg-gray-100 text-black dark:text-white flex flex-col max-w-20"
+                    style={{ wordWrap: "break-word" }}
+                    className="px-2 py-4 dark:bg-gray-800 bg-gray-100 text-black dark:text-white  max-w-20"
                   >
-                    {income.source}
-                    <span
-                      className={`${
-                        income.type === "Expense"
-                          ? "text-red-500"
-                          : "text-green"
-                      }`}
-                    >
-                      {income.type}
-                    </span>
+                    <Tooltip
+                      text={
+                        <>
+                          <div className="flex flex-col">
+                            {income.source}
+                            <span
+                              className={`${
+                                income.type === "Expense"
+                                  ? "text-red-500"
+                                  : "text-green"
+                              }`}
+                            >
+                              {income.type}
+                            </span>
+                          </div>
+                        </>
+                      }
+                      children={income.note}
+                    />
                   </th>
-                  <td style={{ wordWrap:"break-word" }} className="px-2 py-4 dark:bg-gray-700 max-w-16">{income.amount}</td>
+                  <td
+                    style={{ wordWrap: "break-word" }}
+                    className="px-2 py-4 dark:bg-gray-700 max-w-16"
+                  >
+                    {income.amount}
+                  </td>
                   <td className="px-2 py-4 dark:bg-gray-800 bg-gray-100 text-black dark:text-white">
                     {income.type === "Budget" ? income.month : income.date}
                   </td>
-                    
-                    <td className="pr-4 py-4 dark:bg-gray-800 bg-gray-100 text-black dark:text-white ">
+
+                  <td className="pr-4 py-4 dark:bg-gray-800 bg-gray-100 text-black dark:text-white ">
                     <button
                       onClick={() =>
                         dispatch(
@@ -67,9 +83,7 @@ const ReportTable = ({ tabledata, type }) => {
                       }
                       className="userButton rounded cursor-pointer"
                     >
-                      <Trash2 className="text-red-500" size={20}/>
-
-                      
+                      <Trash2 className="text-red-500" size={20} />
                     </button>
                   </td>
                 </tr>
@@ -92,3 +106,25 @@ const ReportTable = ({ tabledata, type }) => {
 };
 
 export default ReportTable;
+
+const Tooltip = ({ text, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+      className="relative flex justify-center items-center"
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+    >
+      {text}
+      {isVisible && (
+        <div>
+          <div className="absolute max-w-44 bottom-[40px] ml-2 transform -translate-x-1/2 dark:bg-gray-700 bg-gray-200 py-2 px-4 rounded-md border-transparent border-t-gray-800">
+            {" "}
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin'; // Correctly import Flowbite
 export default {
   darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite/**/*.js', // Make sure Flowbite content path is correct
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailgrids/plugin")],
+  plugins: [
+    require("tailgrids/plugin"), // Ensure Tailgrids is installed and available
+    flowbite, // Use Flowbite correctly as a plugin
+  ],
 }
-
