72adba0f4f63c5588af90d3953eb1b257aa4dafd
touhidul-islam-html <ti360109@gmail.com>
2024-10-14 12:50:00 +0600

    update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,15 +4,19 @@ import Summary from "../component/Summary";
 import { useSelector } from "react-redux";
 
 const Layout = () => {
-    const incomeList = useSelector((state) => state.personalBanking.addIncome);
-    const expenseList = useSelector((state) => state.personalBanking.addExpense);
-    const budgetList = useSelector((state) => state.personalBanking.addBudget);
-    return (
-        <div>
-            <Summary incomeList={incomeList} expenseList={expenseList} budgetList={budgetList} />
-            <SelectedDate incomeList={incomeList} expenseList={expenseList} />
-        </div>
-    );
+  const incomeList = useSelector((state) => state.personalBanking.addIncome);
+  const expenseList = useSelector((state) => state.personalBanking.addExpense);
+  const budgetList = useSelector((state) => state.personalBanking.addBudget);
+  return (
+    <div>
+      <Summary
+        incomeList={incomeList}
+        expenseList={expenseList}
+        budgetList={budgetList}
+      />
+      <SelectedDate incomeList={incomeList} expenseList={expenseList} />
+    </div>
+  );
 };
 
 export default Layout;
